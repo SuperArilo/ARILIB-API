a25d3869ef80d550f1e7d5425b0406398d32be0c
Arilo <superarilowh@gmail.com>
2026-02-24 21:21:51 +0800

    移除不必要的

--- src/main/java/com/tty/api/annotations/entity/CacheKey.java
+++
@@ -1,8 +0,0 @@
-package com.tty.api.annotations.entity;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface CacheKey { }
