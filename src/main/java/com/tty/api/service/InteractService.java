aa0c8365133b01c45a1df82cbabe4658c8a22066
SuperArilo <728152568@qq.com>
2026-04-14 17:15:44 +0800

    充值交互检查逻辑为独立的 HOOK 模式（未完成

---
+++ src/main/java/com/tty/api/service/InteractService.java
@@ -0,0 +1,20 @@
+package com.tty.api.service;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public interface InteractService {
+
+    boolean canBuild(Location location);
+
+    boolean canBuild(Location location, Player player);
+
+    boolean canTeleport(Location location);
+
+    boolean canTeleport(Location location, Player player);
+
+    boolean canInteract(Location location);
+
+    boolean canInteract(Location location, Player player);
+
+}
