2155a260280d273dd3df0e0458f89e91a7639482
SuperArilo <728152568@qq.com>
2026-05-28 16:05:10 +0800

    update

--- src/main/java/com/tty/api/dto/gui/BaseItem.java
+++ src/main/java/com/tty/api/dto/gui/BaseItem.java
@@ -1,6 +1,9 @@
 package com.tty.api.dto.gui;
 
+import com.google.gson.annotations.Expose;
 import lombok.Data;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -9,6 +12,10 @@ import java.util.List;
 @Data
 public class BaseItem implements Serializable {
     private String name;
+    @Nullable
+    @Expose
+    /* 直接设置 item，不会被 yml 解析 */
+    private ItemStack itemStack;
     private String material;
     private List<Integer> slot;
     private List<String> lore = new ArrayList<>();
