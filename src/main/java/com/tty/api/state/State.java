97ee4fabea137f068903fc8bed52cf9cdbcb6b6e
SuperArilo <superarilowh@gmail.com>
2026-06-20 10:14:28 +0800

    添加玩家输入指令和聊天的冷却时间限制

--- src/main/java/com/tty/api/state/State.java
+++ src/main/java/com/tty/api/state/State.java
@@ -51,6 +51,10 @@ public class State {
         return this.count.get() >= this.max_count;
     }
 
+    public int getRemain() {
+        return this.max_count - this.count.get();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
