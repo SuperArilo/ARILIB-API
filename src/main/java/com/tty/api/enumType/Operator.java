b7a1429d059302e16ac497df694a9ea6f7eb4e85
SuperArilo <728152568@qq.com>
2026-07-14 12:22:09 +0800

    更新操作员的判断

--- src/main/java/com/tty/api/enumType/Operator.java
+++ src/main/java/com/tty/api/enumType/Operator.java
@@ -2,16 +2,27 @@ package com.tty.api.enumType;
 
 import lombok.Getter;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public enum Operator {
 
-    CONSOLE("00000000-0000-0000-0000-000000000000");
+    CONSOLE("00000000-0000-0000-0000-000000000000"),
+    RCON("11111111-0000-0000-0000-000000000000");
 
     @Getter
     private final String uuid;
+    private static final Map<String, Operator> UUID_TO_OPERATOR = new HashMap<>();
+
+    static {
+        for (Operator op : values()) {
+            UUID_TO_OPERATOR.put(op.uuid, op);
+        }
+    }
 
     Operator(String uuid) {
         this.uuid = uuid;
@@ -20,7 +31,25 @@ public enum Operator {
     public static UUID getOperator(CommandSender sender) {
         if (sender instanceof Player p) {
             return p.getUniqueId();
+        } else if (sender instanceof RemoteConsoleCommandSender) {
+            return UUID.fromString(RCON.getUuid());
+        } else {
+            return UUID.fromString(CONSOLE.getUuid());
         }
-        return UUID.fromString(CONSOLE.getUuid());
     }
+
+    public static Operator fromUuid(String uuidStr) {
+        if (uuidStr == null) {
+            return null;
+        }
+        return UUID_TO_OPERATOR.get(uuidStr);
+    }
+
+    public static Operator fromUuid(UUID uuid) {
+        if (uuid == null) {
+            return null;
+        }
+        return fromUuid(uuid.toString());
+    }
+
 }
