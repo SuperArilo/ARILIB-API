7147129ab00c97941421d93982b981edf31c7b42
SuperArilo <superarilowh@gmail.com>
2026-07-17 16:17:58 +0800

    update

--- src/main/java/com/tty/api/NbtManager.java
+++ src/main/java/com/tty/api/NbtManager.java
@@ -2,6 +2,7 @@ package com.tty.api;
 
 import com.tty.api.enumType.NbtEnum;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
@@ -22,14 +23,18 @@ public class NbtManager {
     }
 
     @Nullable
-    public <T> T getNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<T, T> type) {
+    public <P, C> C getNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<P, C> type) {
         return itemStack.getItemMeta().getPersistentDataContainer().get(this.build(key), type);
     }
 
-    public <T> void setNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<T, T> type, @NotNull T value) {
+    public <P, C> void setNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<P, C> type, @NotNull C value) {
         ItemMeta itemMeta = itemStack.getItemMeta();
         itemMeta.getPersistentDataContainer().set(this.build(key), type, value);
         itemStack.setItemMeta(itemMeta);
+    }
+
+    public <P, C> void setNbt(NbtEnum key, @NotNull Player player, PersistentDataType<P, C> type, @NotNull C value) {
+        player.getPersistentDataContainer().set(this.build(key), type, value);
     }
 
     public void removeNbt(NbtEnum key, @NotNull ItemStack itemStack) {
@@ -38,14 +43,22 @@ public class NbtManager {
         itemStack.setItemMeta(itemMeta);
     }
 
+    public void removeNbt(NbtEnum key, @NotNull Player player) {
+        player.getPersistentDataContainer().remove(this.build(key));
+    }
+
     public boolean hasNbt(NbtEnum key, @NotNull ItemStack itemStack) {
         ItemMeta itemMeta = itemStack.getItemMeta();
         return itemMeta.getPersistentDataContainer().has(this.build(key));
     }
 
-    public <T> boolean hasNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<T, T> type) {
+    public <P, C> boolean hasNbt(NbtEnum key, @NotNull ItemStack itemStack, PersistentDataType<P, C> type) {
         ItemMeta itemMeta = itemStack.getItemMeta();
         return itemMeta.getPersistentDataContainer().has(this.build(key), type);
+    }
+
+    public boolean hasNbt(NbtEnum key, @NotNull Player player) {
+        return player.getPersistentDataContainer().has(this.build(key));
     }
 
     private NamespacedKey build(NbtEnum key) {
