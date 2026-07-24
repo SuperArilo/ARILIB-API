b3d8f751fdb350416e59342d36a4fcd70ec5afff
SuperArilo <superarilowh@gmail.com>
2026-07-24 18:44:52 +0800

    添加占位符构建方法

--- src/main/java/com/tty/api/enumType/PlaceholderTypeEnum.java
+++ src/main/java/com/tty/api/enumType/PlaceholderTypeEnum.java
@@ -2,4 +2,13 @@ package com.tty.api.enumType;
 
 public interface PlaceholderTypeEnum {
     String getType();
+
+    static String testBuild(PlaceholderTypeEnum typeEnum) {
+        return "<" + typeEnum.getType() + ">";
+    }
+
+    static String testBuild(String content) {
+        return "<" + content + ">";
+    }
+
 }
