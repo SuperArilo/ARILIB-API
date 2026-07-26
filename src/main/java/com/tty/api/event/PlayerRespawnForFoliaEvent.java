0b40de188072688200b740172bcddc2ba2a75c55
SuperArilo <superarilowh@gmail.com>
2026-07-26 18:42:33 +0800

    移除不必要的逻辑

--- src/main/java/com/tty/api/event/PlayerRespawnForFoliaEvent.java
+++ src/main/java/com/tty/api/event/PlayerRespawnForFoliaEvent.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class PlayerRespawnForFoliaEvent extends PlayerEvent implements Cancellable {
+public class PlayerRespawnForFoliaEvent extends PlayerEvent {
     @Getter
     private final static HandlerList handlerList = new HandlerList();
     @Getter
@@ -18,22 +17,13 @@ public class PlayerRespawnForFoliaEvent extends PlayerEvent implements Cancellab
     @Setter
     @Getter
     private Location deathLocation;
-    private boolean isCancelled = false;
+
     public PlayerRespawnForFoliaEvent(@NotNull Player who, @NotNull Location respawnLocation, @NotNull Location deathLocation) {
         super(who);
         this.respawnLocation = respawnLocation;
         this.deathLocation = deathLocation;
     }
 
-    @Override
-    public boolean isCancelled() {
-        return isCancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean b) {
-        this.isCancelled = b;
-    }
     @Override
     public @NotNull HandlerList getHandlers() {
         return handlerList;
