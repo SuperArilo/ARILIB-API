52880ea3f51c759563d3f60c54298f7f99bae95f
SuperArilo <728152568@qq.com>
2026-07-28 08:29:53 +0800

    修复没有 PlaceholderAPI 的时候启动报错

--- src/main/java/com/tty/api/AbstractJavaPlugin.java
+++ src/main/java/com/tty/api/AbstractJavaPlugin.java
@@ -110,16 +110,15 @@ public abstract class AbstractJavaPlugin extends JavaPlugin {
         this.statusManager = new StatusManager();
         this.statusManager.registerStateMachine(this.services());
 
-        this.engine = new PlaceholderEngineImpl(this.placeholders());
-
-        List<PlaceholderExpansion> expansions = this.expansions();
-        if (expansions != null && Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+        if (Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            List<PlaceholderExpansion> expansions = this.expansions();
+            if (expansions == null) return;
             for (PlaceholderExpansion expansion : expansions) {
                 expansion.register();
                 this.getLog().debug("register expansion {}", expansion.getClass().getName());
             }
         }
-
+        this.engine = new PlaceholderEngineImpl(this.placeholders());
         this.checkVersionTask = this.createCheckVersionTask();
     }
 
